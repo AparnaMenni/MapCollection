6e49f7e379fae4c7b70aadbcd91a84f1da7abdeb
HOME <HOME@HOME-PC>
2020-07-22 10:10:27 +0530

    initial commit

---
+++ src/map/com/HaspMap.java
@@ -0,0 +1,22 @@
+package map.com;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HaspMap {
+
+	public static void main(String[] args)  
+	{ 
+		Map<Integer,String> m=new HashMap<Integer,String>(); 
+		 
+		m.put(0,"Apps"); 
+		m.put(1,"Harathi"); 
+		m.put(2,"Menni"); 
+		m.put(3,"Anblicks");  
+		System.out.println("The value of m is"+m);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
